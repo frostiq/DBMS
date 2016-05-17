bfe38d6f4baf943240da203b059923eee362b53a
frostiq <sayshayx@gmail.com>
2016-05-17 20:52:34 +0300

    added MSSQK repo

---
+++ DBMS_lab2/DAL/MSSQL/MSSQLFilmstoreRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace DBMS_lab2.DAL.MSSQL
+{
+    public class MSSQLFilmstoreRepository : BaseFilmstoreRepository
+    {
+        private SqlConnection connection;
+
+        public MSSQLFilmstoreRepository(string connectionString) : this(new SqlConnection(connectionString))
+        {
+        }
+
+        private MSSQLFilmstoreRepository(SqlConnection connection) : base(connection)
+        {
+            this.connection = connection;
+        }
+
+        internal SqlDataAdapter GetDataAdapter(string tableName)
+        {
+            var adapter = new SqlDataAdapter($"SELECT * FROM {tableName}", connection);
+            return adapter;
+        }
+    }
+}
